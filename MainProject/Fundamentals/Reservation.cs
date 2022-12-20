cd7b631b24818f500e2234e929afe7ed88e92bb1
Ricardo Pineda <ricardo@Ricardos-MacBook-Pro.local>
2022-12-19 22:56:36 -0500

    Base project for unit testing study with NUnit

---
+++ MainProject/Fundamentals/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MainProject.Fundamentals
+{
+	public class Reservation
+	{
+		public User? MadeBy { get; set; }
+
+		public bool CanBeCancelledBy(User user)
+		{
+			return (user.IsAdmin || MadeBy == user);
+        }
+    }
+
+	public class User
+	{
+		public bool IsAdmin { get; set; }
+	}
+}
+
---
+++ TestProject/ReservationTest.cs
@@ -0,0 +1,62 @@
+﻿using MainProject.Fundamentals;
+using NUnit.Framework;
+
+namespace TestProject;
+
+
+[TestFixture]
+public class ReservationTest
+{
+    [Test]
+    public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+    {
+        //arreange
+        Reservation reservation = new Reservation();
+
+        //act
+        var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+        //assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_ReservationMadeByUser_ReturnsTrue()
+    {
+        //arreange
+        Reservation reservation = new();
+        User user = new();
+        reservation.MadeBy = user;
+        //act
+        var result = reservation.CanBeCancelledBy(user);
+
+        //assert
+        Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_NonAdminUser_ReturnsFalse()
+    {
+        //arreange
+        Reservation reservation = new();
+        User user = new() { IsAdmin = false};
+        //act
+        var result = reservation.CanBeCancelledBy(user);
+
+        //assert
+        Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void CanBeCancelledBy_NoMadeByUser_ReturnsFalse()
+    {
+        //arreange
+        Reservation reservation = new();
+        User user = new() {};
+        //act
+        var result = reservation.CanBeCancelledBy(user);
+
+        //assert
+        Assert.That(result, Is.False);
+    }
+}
