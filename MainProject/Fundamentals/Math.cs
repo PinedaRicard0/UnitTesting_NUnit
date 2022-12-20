030d4d146feae1f6c2eefa0f53aadde06dc69618
“Ricardo <pinedaricardo07@gmail.com>
2022-12-20 00:08:12 -0500

    Adding math class and basic tests

---
+++ MainProject/Fundamentals/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MainProject.Fundamentals
+{
+	public class Math
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+        public int Max(int a, int b)
+        {
+			return (a > b) ? a : b;
+        }
+    }
+}
+
---
+++ TestProject/MathTtests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+namespace TestProject
+{
+	[TestFixture]
+	public class MathTtests
+	{
+        private  MainProject.Fundamentals.Math? _math;
+
+        /// <summary>
+        /// This Decorator indicates that the logic inside will run before every
+        /// test you have
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new MainProject.Fundamentals.Math();
+        }
+
+		[Test]
+		public void Add_WhenCalled_ReturnSumOfArguments()
+		{
+			var result = _math?.Add(5, 3);
+
+			Assert.That(result, Is.EqualTo(8));
+		}
+
+        [Test]
+        public void Max_WhenFirstArgumentIsGraeterThanSecond_ReturnFirstArgument()
+        {
+            var result = _math?.Max(5, 3);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Max_WhenSecondArgumentIsGraeterThanFirst_ReturnSecondArgument()
+        {
+            var result = _math?.Max(3, 5);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Max_WhenArgumentsAreEqual_ReturnSameArgument()
+        {
+            var result = _math?.Max(5, 5);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
+
